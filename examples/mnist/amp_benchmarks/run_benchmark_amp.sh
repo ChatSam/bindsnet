66c56be95db74657e69d7f15f78659153508f692
Chatura Samarasinghe <chatura@iteratestudio.com>
2021-05-16 17:06:25 -0700

    Global AMP testing for batch_eth_mnist

--- examples/mnist/amp_benchmarks/run_benchmark_amp.sh
+++ examples/mnist/amp_benchmarks/run_benchmark_amp.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/bash
 
 #python ../batch_eth_mnist.py --gpu >> no_amp_baseline_2.txt
-python ../batch_eth_mnist.py --gpu --amp >> amp_nodes.txt
+#python ../batch_eth_mnist.py --gpu --amp >> amp_nodes.txt
 
-#sudo shutdown
+# # 2000 neurons
+# python ../batch_eth_mnist.py --gpu --n_neurons 2000 >> no_amp_baseline_2000_neurons.txt
+# python ../batch_eth_mnist.py --gpu --amp --n_neurons 2000 >> amp_nodes_2000_neurons.txt
+
+python ../batch_eth_mnist.py --gpu --amp --n_neurons 4000 >> amp_nodes_4000_neurons.txt
+
+python ../batch_eth_mnist.py --gpu --n_neurons 4000 >> no_amp_baseline_4000_neurons.txt
+
+
+sudo shutdown
